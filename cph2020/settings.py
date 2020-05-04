d265dd5f3c2fa8d47c9b242d927ab4d842f4b584
jesse <Jesse.Orlowski@carvana.com>
2020-05-03 19:23:49 -0500

    Staticfiles take3 whitenoise

--- cph2020/settings.py
+++ cph2020/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cph2020.urls'
@@ -120,4 +121,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
